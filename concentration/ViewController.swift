4cc0171e28370dd61c4ada00b00c1f47cf3c9146
sitnikov <oleg.sitnikov@icloud.com>
2019-12-31 09:15:25 +0300

    Initial Commit

---
+++ concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Oleg Sitnikov on 31.12.2019.
+//  Copyright © 2019 Oleg Sitnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
